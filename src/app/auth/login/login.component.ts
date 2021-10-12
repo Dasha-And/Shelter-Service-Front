9db668b3cdf8ae2aa61e205f27059bc289f815ed
navwie <aliesia.lykhova@nure.ua>
2021-10-12 13:43:31 +0300

    initial commit

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  flag: boolean = true;
+
+
+  apply(value: string) {
+    this.flag = value === "login" ? true : false;
+  }
+  successMessage: string = " ";
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern("[A-Za-z0-0]*@gmail.com")]],
+      password: ['', [Validators.required, Validators.pattern("[A-Za-z0-9@!_]{6,}")]]
+    })
+  }
+  login() {
+    this.successMessage = "Successfully Loggined In ..."
+  }
+}
---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  flag: boolean = true;
+
+
+  apply(value: string) {
+    this.flag = value === "login" ? true : false;
+  }
+  successMessage: string = " ";
+  regForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.regForm = this.fb.group({
+      name: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      mobileNumber: ['', [Validators.required, Validators.pattern(new RegExp("[0-9 ]{12}"))]],
+      email: ['', [Validators.required, Validators.pattern("[A-Za-z0-0]*@gmail.com")]],
+      password: ['', [Validators.required, Validators.pattern("[A-Za-z0-9@!_]{6,}")]],
+      admin: []
+    })
+  }
+  register() {
+    this.successMessage = "Successfully register In ..."
+    console.log(this.regForm)
+  }
+
+}
