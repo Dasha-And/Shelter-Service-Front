0094ecbeb626c178c68161d5337a0a513544d7ee
navwie <aliesia.lykhova@nure.ua>
2021-10-22 18:08:14 +0300

    add edit and add form for admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,32 @@ import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminFormRegComponent} from "./Users/admin/adminFormReg/admin-form-reg/admin-form-reg.component";
-import { AdminComponent} from "./Users/admin/admin.component";
-import {HttpClientModule} from "@angular/common/http";
-import {UserComponent} from "./Users/user/user.component";
+import { AdminFormRegComponent } from "./Users/admin/adminFormReg/admin-form-reg/admin-form-reg.component";
+import { AdminComponent } from "./Users/admin/admin.component";
+import { HttpClientModule } from "@angular/common/http";
+import { UserComponent } from "./Users/user/user.component";
+import { HeaderAdminComponent } from './headers/headers-admin/header-admin/header-admin.component';
+import { HeaderUnregComponent } from './headers/headerUnreg/header-unreg/header-unreg.component';
+import { ShelterComponent } from './Users/admin/shelter/shelter/shelter.component';
+import { AccountAdminComponent } from './Users/admin/accountAdmin/account-admin/account-admin.component';
+import { ChangeAccountDataComponent } from './Users/admin/changesData/changeAccount/change-account-data/change-account-data.component';
+import { ChangeShelterDataComponent } from './Users/admin/changesData/changeShelter/change-shelter-data/change-shelter-data.component';
 
 
-const appRoutes : Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'contacts', component: ContactsComponent},
-  {path: 'login',component: LoginComponent},
-  {path: 'regist',component: RegisterComponent},
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'regist', component: RegisterComponent },
   { path: 'adminPage', component: AdminComponent },
   { path: 'userPage', component: UserComponent },
   { path: 'adminFormReg', component: AdminFormRegComponent },
+  { path: 'shelter', component: ShelterComponent },
+  { path: 'account', component: AccountAdminComponent },
+  { path: 'changeAccount', component: ChangeAccountDataComponent},
+  { path: 'changeShelter', component: ChangeShelterDataComponent },
+
 ]
 
 @NgModule({
@@ -34,7 +46,14 @@ const appRoutes : Routes = [
     ContactsComponent,
     AdminFormRegComponent,
     AdminComponent,
-    UserComponent
+    UserComponent,
+    HeaderAdminComponent,
+    HeaderUnregComponent,
+    ShelterComponent,
+    AccountAdminComponent,
+    ChangeAccountDataComponent,
+    ChangeShelterDataComponent,
+
 
   ],
   imports: [
