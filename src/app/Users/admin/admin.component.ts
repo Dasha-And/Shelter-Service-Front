64ca19136049045d1802a02d80efb6b45a7f6559
Dasha-And <dosados69@gmail.com>
2021-10-25 00:53:12 +0300

    added rounting to shelter reg form with query params

--- src/app/Users/admin/admin.component.ts
+++ src/app/Users/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {Shelter} from "../../model/shelter";
+import {HttpErrorResponse} from "@angular/common/http";
+import {User} from "../../model/user";
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
+  goToShelterReg() {
+    this.route.queryParams.subscribe(params => {
+      const email = params['email'];
+      this.router.navigate(['/adminFormReg/:email'], {queryParams: {email: email}});
+    });
 
+  }
 }
