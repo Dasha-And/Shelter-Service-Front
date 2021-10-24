b8018af79c8e6977061fd8f5d71bf8fa46c642d2
Dasha-And <dosados69@gmail.com>
2021-10-25 00:54:42 +0300

    implemented shelter registration

--- src/app/Users/admin/adminFormReg/admin-form-reg/admin-form-reg.component.ts
+++ src/app/Users/admin/adminFormReg/admin-form-reg/admin-form-reg.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {User} from "../../../../model/user";
+import {HttpErrorResponse} from "@angular/common/http";
+import {ShelterService} from "../../../../service/shelter.service";
+import {Shelter} from "../../../../model/shelter";
+import {ActivatedRoute, Router} from "@angular/router";
+import {UserService} from "../../../../service/user.service";
 
 @Component({
   selector: 'app-admin-form-reg',
@@ -10,21 +16,46 @@ export class AdminFormRegComponent implements OnInit {
   successMessage: string = " ";
   adminRegForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private shelterService: ShelterService, private route: ActivatedRoute, private router: Router, private userService: UserService) {
   }
 
   ngOnInit(): void {
     this.adminRegForm = this.fb.group({
       name: ['', [Validators.required]],
-      longitude: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
-      latitude: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+      longitude: ['', [Validators.required, Validators.pattern("[+-]?([0-9]*[.])?[0-9]+")]],
+      latitude: ['', [Validators.required, Validators.pattern("[+-]?([0-9]*[.])?[0-9]+")]],
       phone: ['', [Validators.required, Validators.pattern(new RegExp("[0-9 ]{12}"))]],
       email: ['', [Validators.required, Validators.pattern(new RegExp("\\w+@\\w+\\.\\w+"))]],
       siteUrl: ['', [Validators.required, Validators.pattern(new RegExp("https?://.+"))]],
     })
   }
   regForm() {
-    this.successMessage = "Successfully register In ..."
-    console.log(this.adminRegForm)
+    this.route.queryParams.subscribe(params => {
+      const email = params['email'];
+      this.shelterService.addShelter(this.adminRegForm.value, email).subscribe(
+        (response: Shelter) => {
+          console.log(response);
+          this.adminRegForm.reset();
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+          this.adminRegForm.reset();
+        }
+      );
+
+    });
+  }
+  getShelterId() {
+    this.route.queryParams.subscribe(params => {
+      const email = params['email'];
+      let user: User;
+      this.userService.getUserByEmail(email).subscribe(
+        (response: User) => {
+          user = response;
+          console.log(user);
+          this.router.navigate(['account/:userId, shelterId'], {queryParams: {userId: email, shelterId: user.shelterId}})
+        }
+      )
+    });
   }
 }
